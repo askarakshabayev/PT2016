22d33035c5a45d58fcd9b970edba6544fa2806c4
Aza <anuftolla>
2016-01-19 11:52:30 +0600

    added example of FileInfo

---
+++ ExampleLecture2/ExampleLecture2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleLecture2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FileInfo
+            // DirectoryInfo
+
+            FileInfo file = new FileInfo(@"c:\testfolder\in1.txt");
+            if (file.Exists)
+            {
+                Console.WriteLine("File name {0}, File fullname {1}", file.Name, file.FullName);
+            } else
+            {
+                Console.WriteLine("Error");
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- FileExample/FileExample/Program.cs
+++ FileExample/FileExample/Program.cs
@@ -6,24 +6,15 @@ using System.Threading.Tasks;
 
 namespace FileExample
 {
-    class Complex
-    {
-        // a = 5, b = 6 -> 5/6
-        public int a, b;
-
-        public override string ToString()
-        {
-            return a + "/" + b;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Complex c = new Complex();
-            c.a = 5;
-            c.b = 6;
+            Complex c = new Complex(5, 6);
+            Complex d = new Complex(6, 7);
+
+            Complex q = c + d;
+
             Console.WriteLine(c);
             Console.ReadKey();
         }
