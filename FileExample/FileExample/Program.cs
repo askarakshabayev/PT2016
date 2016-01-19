e368e37dfd7e3ce10eb7cafe6a3353c4077e68ee
Aza <anuftolla>
2016-01-19 11:20:26 +0600

    a + b implemented

---
+++ FileExample/FileExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] token = Console.ReadLine().Split();
+            int a = int.Parse(token[0]);
+            int b = int.Parse(token[1]);
+            int c = a + b;
+            Console.WriteLine(c);
+            Console.WriteLine("hello world");
+            Console.ReadKey();
+        }
+    }
+}
