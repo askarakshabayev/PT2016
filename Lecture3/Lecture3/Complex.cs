a3d8a2adf961801885741da4a449c9ca596eb907
Aza <anuftolla>
2016-01-21 11:33:36 +0600

    added example of + operator

---
+++ Lecture3/Lecture3/Complex.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+
+        public static int gcd(int a, int b)
+        {
+            // a = 24 -> 0
+            // b = 36 -> 12
+            while (a > 0 && b > 0)
+            {
+                if (a < b)
+                    b = b % a;
+                else
+                    a = a % b;
+            }
+            return a + b;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3 = new Complex(c1.a + c2.a, c1.b + c2.b);
+            //int g = gcd(c3.a, c3.b);
+            //c3.a /= g;
+            //c3.b /= g;
+            return c3;
+        }
+    }
+}
---
+++ Lecture3/Lecture3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex(5, 6);
+            Console.WriteLine(c);
+            Complex d = c + c;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
