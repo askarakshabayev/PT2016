935dc71e2277ae17a19515e94b18480502b5c9c6
Aza <anuftolla>
2016-01-21 12:21:33 +0600

    added example of iterating through directory

--- Lecture3/Lecture3/Program.cs
+++ Lecture3/Lecture3/Program.cs
@@ -9,20 +9,46 @@ namespace Lecture3
 {
     class Program
     {
+        static void WalkTreeDirectory(DirectoryInfo d, int depth)
+        {
+            try {
+                // list all files
+                foreach (FileInfo file in d.GetFiles())
+                {
+                    for (int i = 0; i < 2 * depth; i++)
+                        Console.Write(" ");
+                    Console.WriteLine("Depth: {0}, Name: {1}", depth, file.Name);
+                }
+
+                // list all folders
+                foreach (DirectoryInfo directory in d.GetDirectories())
+                {
+                    for (int i = 0; i < 2 * depth; i++)
+                        Console.Write(" ");
+                    Console.WriteLine("Depth: {0}, Name: {1}", depth, directory.Name);
+                    WalkTreeDirectory(directory, depth + 1);
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        // testfolder
+        // --in1.txt
+        // --in2.txt
+        // --test
+        // ----a.txt
+        // ----b.txt
+        // ----c
+        // ------q.txt
+        // ------w.txt
+
         static void Main(string[] args)
         {
-            // FileInfo
-            // DirectoryInfo
-
-            FileInfo f = new FileInfo(@"c:\testfolder\in1.txt");
-            if (f.Exists)
-            {
-                //Console.WriteLine("File name " + f.Name + " Size " + f.Length);
-                Console.WriteLine("File name {0}, Size {1}", f.Name, f.Length);
-                Console.WriteLine("File fullname {0}", f.FullName);
-            }
-            
-
+            DirectoryInfo d = new DirectoryInfo(@"c:\windows");
+            WalkTreeDirectory(d, 1);
             Console.ReadKey();
         }
     }
