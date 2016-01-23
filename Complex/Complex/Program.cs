2ca93b890c6ecd59a5b95d66ab14a530fd488a98
Aza <anuftolla>
2016-01-23 10:54:24 +0600

    added example of complex number

---
+++ Complex/Complex/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex(5, 6);
+            Complex d = new Complex(6, 7);
+            Complex e = ((c + d) + c) + d;
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    }
+}
