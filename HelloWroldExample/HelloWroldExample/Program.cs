e11273bf4ce83cce177c56051f2089aa96b306f0
Aza <anuftolla>
2016-01-23 10:47:11 +0600

    added Student class

--- HelloWroldExample/HelloWroldExample/Program.cs
+++ HelloWroldExample/HelloWroldExample/Program.cs
@@ -6,14 +6,40 @@ using System.Threading.Tasks;
 
 namespace HelloWroldExample
 {
+    class Student
+    {
+        // fields
+        // methods
+        public string name;
+        public string surname;
+        public double gpa;
+        public string ID;
+
+        public Student(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public Student(string name, string surname, double gpa)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.gpa = gpa;
+        }
+
+        public override string ToString()
+        {
+            return name + " " + surname;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            int a = int.Parse(Console.ReadLine());
-            int b = int.Parse(Console.ReadLine());
-            int c = a + b;
-            Console.WriteLine(c);
+            Student a = new Student("Aslan", "Mussin");
+            Console.WriteLine(a);
 
             Console.ReadKey();
         }
