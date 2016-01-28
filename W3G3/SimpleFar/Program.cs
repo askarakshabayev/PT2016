59533cb2a2cd435a2dd4efed6922ad6ddf321890
Aza <anuftolla>
2016-01-28 12:15:16 +0600

    added example of simple far

---
+++ W3G3/SimpleFar/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"c:\testfolder");
+
+            int index = 0;
+            FileInfo[] files = d.GetFiles();
+
+            while (true)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if (index == i)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine(files[i].Name);
+                }
+
+                ConsoleKeyInfo button = Console.ReadKey();
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    if (index > 0)
+                        index--;
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    if (index < files.Length - 1)
+                        index++;
+                }
+                if (button.Key == ConsoleKey.Escape)
+                    break;
+
+                Console.Clear();
+            }
+            Console.ReadKey();
+        }
+    }
+}
