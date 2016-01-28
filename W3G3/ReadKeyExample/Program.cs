a55c6a03a6652ec712bf8e460b03d3559da2999c
Aza <anuftolla>
2016-01-28 12:02:54 +0600

    added example of readkey

---
+++ W3G3/ReadKeyExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadKeyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKeyInfo button = Console.ReadKey();
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Up");
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Down");
+                }
+                if (button.Key == ConsoleKey.LeftArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Left");
+                }
+                if (button.Key == ConsoleKey.RightArrow)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("Right");
+                }
+                if (button.Key == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                if (button.Key == ConsoleKey.Enter)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Clear();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- W3G3/W3G3/Program.cs
+++ W3G3/W3G3/Program.cs
@@ -9,23 +9,52 @@ namespace W3G3
 {
     class Program
     {
-        static int FilesCnt(DirectoryInfo directory)
-        {
-            FileInfo[] files = directory.GetFiles();
-            int cnt = files.Length;
-            // cnt = 2
-            foreach(DirectoryInfo d in directory.GetDirectories())
-            {
-                cnt += FilesCnt(d);
-            }
-
-            return cnt;
-        }
         static void Main(string[] args)
         {
-            DirectoryInfo directory = new DirectoryInfo(@"c:\testfolder");
-            int cnt = FilesCnt(directory);
-            Console.WriteLine(cnt);
+        }
+        static void F3()
+        {
+            FileStream finput = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream foutput = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(finput);
+            StreamWriter sw = new StreamWriter(foutput);
+
+            string s = sr.ReadLine();
+            string[] token = s.Split(',');
+            // token[0] = 1
+            // token[1] = 12 ...
+            int sum = 0;
+            foreach(string q in token)
+            {
+                sum += int.Parse(q);
+            }
+
+            sw.WriteLine(sum);
+
+            sw.Close();
+            sr.Close();
+            finput.Close();
+            foutput.Close();
+        }
+        static void F2()
+        {
+            FileStream fs = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            string s = Console.ReadLine();
+
+            sw.WriteLine(s);
+            Console.ReadKey();
+            sw.Close();
+            fs.Close();
+        }
+        static void F1()
+        {
+            // FileStream, StreamWriter, StreamReader
+            // file name, FileMode, FileAccess
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            string text = sr.ReadToEnd();
+            Console.WriteLine(text);
 
             Console.ReadKey();
         }
