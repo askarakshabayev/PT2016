62f91cdb133a21ef22b2a4c2733b237f645a6803
Aza <anuftolla>
2016-01-30 11:08:46 +0600

    added example of reading and writing to file

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    // f("testfolder") = count_of_files + f("sdf") + f("test") + f("test1)
+    // f("test1") = count_of_files + f("aaa") 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F3();
+            Console.ReadKey();
+        }
+
+        static void F3()
+        {
+            FileStream fread = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fwrite = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            StreamReader sr = new StreamReader(fread);
+            StreamWriter sw = new StreamWriter(fwrite);
+
+            string[] token = sr.ReadLine().Split(',');
+            // token[0] = 1
+            // token[1] = 23 ...
+            int sum = 0;
+            for (int i = 0; i < token.Length; i++)
+            {
+                sum += int.Parse(token[i]);
+            }
+            sw.WriteLine(sum);
+
+            sw.Close();
+            sr.Close();
+            fwrite.Close();
+            fread.Close();
+        }
+
+        static void F2()
+        {
+            FileStream fs = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("hello world");
+            sw.Close();
+            fs.Close();                
+        }
+        static void F1()
+        {
+            // FileStream
+            // StreamReader
+            // StreamWriter
+            FileStream fs = new FileStream(@"c:\testfolder\input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            string s = sr.ReadToEnd();
+            Console.WriteLine(s);
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
---
+++ W3G3/ConsoleApplication1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[100];
+            int[] y = new int[100];
+            int len = 1;
+            x[0] = 0; y[0] = 0;
+            int k = 0;
+
+            while (true)
+            {
+                for (int i = 0; i < len; i++)
+                {
+                    Console.SetCursorPosition(x[i], y[i]);
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.Write(" ");
+                }
+                k++;
+                if (k % 5 == 0)
+                    len++;
+
+                for (int i = len - 1; i >= 1; i--)
+                {
+                    x[i] = x[i - 1];
+                    y[i] = y[i - 1];
+                }
+
+                ConsoleKeyInfo button = Console.ReadKey();
+                if (button.Key == ConsoleKey.UpArrow)
+                {
+                    if (y[0] > 0)
+                       y[0]--;
+                }
+                if (button.Key == ConsoleKey.DownArrow)
+                {
+                    if (y[0] < Console.WindowHeight - 2)
+                        y[0]++;
+                }
+                if (button.Key == ConsoleKey.LeftArrow)
+                    if (x[0] > 0)
+                        x[0]--;
+                if (button.Key == ConsoleKey.RightArrow)
+                    if (x[0] < Console.WindowWidth - 2)
+                        x[0]++;
+                if (button.Key == ConsoleKey.Escape)
+                    break;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Clear();                       
+            }
+            Console.ReadKey();
+        }
+    }
+}
