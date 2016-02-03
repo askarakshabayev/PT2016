6765869688f44bff6963454b758059473ee57a18
Aza <anuftolla>
2016-02-03 14:37:01 +0600

    added Example of xml serialization and snake

---
+++ SnakeW4G2/SnakeW4G2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeW4G2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Models.Game game = new Models.Game();
+        }
+    }
+}
---
+++ XmlSerializationExample/XmlSerializationExample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlSerializationExample
+{
+    public class Point
+    {
+        public int x, y;
+        private int z;
+        public Info info = new Info();
+
+        public Point() { }
+
+        public Point(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+    public class Info
+    {
+        public string owner, version;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        }
+        static void F1()
+        {
+            FileStream fs = new FileStream("state.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            Point a = new Point(3, 4, 5);
+            a.info.owner = "ABC";
+            a.info.version = "1.0";
+            XmlSerializer xs = new XmlSerializer(typeof(Point));
+            xs.Serialize(fs, a);
+
+            fs.Close();
+        }
+
+        static void F2()
+        {
+            FileStream fs = new FileStream("state.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Point));
+            Point b = xs.Deserialize(fs) as Point;
+            Console.WriteLine(b.info.version);
+            Console.WriteLine(b.x);
+            Console.ReadKey();
+            fs.Close();
+        }
+    }
+}
